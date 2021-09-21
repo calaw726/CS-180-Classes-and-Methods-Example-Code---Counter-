375dbe5f8c290e394de115e0382dd9b2a505f84d
calaw726 <89656489+calaw726@users.noreply.github.com>
2021-09-21 09:32:50 -0400

    Add files via upload

---
+++ Counter.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Counter {
+    int x;
+    static int y;
+
+    Counter(int x) {
+        this.x = x;
+    }
+
+    static void display(String name, Counter c) {
+        System.out.printf("%s: x = %d,  y = %d\n", name, c.x, c.y);
+    }
+    public static void main(String[] args) {
+        Counter alice = new Counter(100);
+        Counter jimmy = new Counter(500);
+
+        display("alice", alice);
+        display("jimmy", jimmy);
+
+        alice.x++;
+        alice.y++;
+        jimmy.x++;
+        jimmy.y++;
+
+        display("alice", alice);
+        display("jimmy", jimmy);
+    }
+}
